d62564d88ac53d9d9d07ceeebb36688401acb8e8
Richard O. Amoako Agyei <richard.agyei@thomsonreuters.com>
2017-05-01 16:58:01 -0500

    Docker build to show container runs

--- alpine-node-example/routes/index.js
+++ alpine-node-example/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', cloud: process.env.CLOUD_ENV || 'Local' });
 });
 
 module.exports = router;
