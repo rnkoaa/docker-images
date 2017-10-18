79fe7cac7cc05b13511484e2f711f0fcdc2be76f
Richard.AmoakoAgyei <richard.amoakoagyei@target.com>
2017-10-18 16:44:18 -0500

    Changes to security and user creation on initial docker container startup

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -6,15 +6,33 @@ import jenkins.security.s2m.AdminWhitelistRule
  
 def instance = Jenkins.getInstance()
  
- def user = 'admin', pass = 'admin'
+def user = 'admin', pass = 'admin'
 
- def jenkinsUserFile = new File("/run/secrets/jenkins-user")
- if(jenkinsUserFile.exists())
- 	user = jenkinsUserFile.text().trim()
+// first try to see if the jenkins username was mounted as a docker secret
+// if not, try to see if it was passed in as an environment variable
+// Lastly the least safe is to use the hard coded variable
+def jenkinsUserFile = new File("/run/secrets/jenkins-user")
+if(jenkinsUserFile.exists())
+	user = jenkinsUserFile.text().trim()
+else {
+	// try to get it from environment variables
+	def adminUser = System.getenv("JENKINS_ADMIN_USER")
+	if(adminUser != null && adminUser.size() > 0)
+		user = adminUser
+}
 
- def jenkinsPassFile = new File("/run/secrets/jenkins-pass")
- if(jenkinsPassFile.exists())
+// first try to see if the jenkins password was mounted as a docker secret
+// if not, try to see if it was passed in as an environment variable
+// Lastly the least safe is to use the hard coded variable
+def jenkinsPassFile = new File("/run/secrets/jenkins-pass")
+if(jenkinsPassFile.exists())
  	pass = jenkinsPassFile.text().trim()
+else {
+ 	// try to get it from environment variables
+ 	def adminPass = System.getenv("JENKINS_ADMIN_PASSWORD")
+ 	if(adminPass != null && adminPass.size() > 0)
+ 		pass = adminPass
+ }
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
