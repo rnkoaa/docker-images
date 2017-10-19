21628b4196ac90d0cd8fdcbbf6d6dc276e4caa19
Richard.AmoakoAgyei <richard.amoakoagyei@target.com>
2017-10-19 09:39:19 -0500

    remove env

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -2,11 +2,20 @@
  
 import jenkins.model.*
 import hudson.security.*
+import java.util.logging.LogManager
 import jenkins.security.s2m.AdminWhitelistRule
  
+
+def logger = LogManager.getLogManager().getLogger("")
 def instance = Jenkins.getInstance()
- 
-def user = 'admin', pass = 'admin'
+def env = System.getenv()
+logger.info("RUNNING security.groovy from ${Jenkins.instance.getRootDir().absolutePath}")
+def user, pass
+
+logger.info("Here are the Jenkins environment variables on startup")
+env.each{
+ logger.info("${it}")
+}
 
 // first try to see if the jenkins username was mounted as a docker secret
 // if not, try to see if it was passed in as an environment variable
@@ -16,9 +25,9 @@ if(jenkinsUserFile.exists())
 	user = jenkinsUserFile.text().trim()
 else {
 	// try to get it from environment variables
-	def adminUser = System.getenv("JENKINS_ADMIN_USER")
-	if(adminUser != null && adminUser.size() > 0)
-		user = adminUser
+	def adminUser =env['JENKINS_ADMIN_USER']
+	user = (adminUser) ? adminUser : 'admin'
+	 logger.info("Setting Admin User as ${user}")
 }
 
 // first try to see if the jenkins password was mounted as a docker secret
@@ -29,9 +38,9 @@ if(jenkinsPassFile.exists())
  	pass = jenkinsPassFile.text().trim()
 else {
  	// try to get it from environment variables
- 	def adminPass = System.getenv("JENKINS_ADMIN_PASSWORD")
- 	if(adminPass != null && adminPass.size() > 0)
- 		pass = adminPass
+ 	def adminPass = env['JENKINS_ADMIN_PASSWORD']
+	 pass = (adminPass) ? adminPass : 'admin'
+	  logger.info("Setting Admin User as ${pass}")
  }
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
